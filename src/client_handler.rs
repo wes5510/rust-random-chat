c2233690ed2c226c23c20118d71ab5596d953508
wes5510 <lkh5510@gmail.com>
2021-01-24 17:20:40 +0900

    refactor(session): update create_session

--- src/client_handler.rs
+++ src/client_handler.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hint::unreachable_unchecked, io::Write, net::TcpStream};
+use std::{collections::HashMap, io::Write, net::TcpStream};
 
 const USER_PREFIX: &'static str = "user";
 
@@ -19,15 +19,18 @@ impl SessionManager {
         };
     }
 
-    pub fn create_session(&mut self, mut stream: TcpStream) {
+    pub fn create_session(&mut self, stream: TcpStream) {
         self.seq = self.seq + 1;
-        self.sessions.insert(
-            self.seq,
-            Session {
-                stream: stream.try_clone().unwrap(),
-            },
-        );
-        stream.write(self.say_hello().as_bytes()).unwrap();
+        let mut newSession = Session {
+            stream: stream.try_clone().unwrap(),
+        };
+
+        newSession
+            .stream
+            .write(self.say_hello().as_bytes())
+            .unwrap();
+
+        self.sessions.insert(self.seq, newSession);
     }
 
     fn say_hello(&self) -> String {
